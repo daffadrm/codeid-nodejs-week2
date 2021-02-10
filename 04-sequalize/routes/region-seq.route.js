f9e50f9f7247db7fa2ea91fa0a489f662a46f7ee
kangdiancp <kangdiancp@gmail.com>
2021-02-10 10:23:18 +0700

    done

--- 04-sequalize/routes/region-seq.route.js
+++ 04-sequalize/routes/region-seq.route.js
@@ -17,12 +17,23 @@ router.get('/:regionId', async (req, res) => {
   return res.send(regions);
 });
 
+// post
 router.post('/', async (req, res) => {
-  const { region_id, region_name} = req.body;
+  const { region_id, region_name } = req.body;
   const regions = await req.context.models.Regions.create({
     region_id: region_id,
     region_name: region_name,
   });
+
+  return res.send(regions);
+});
+
+// put
+router.put('/:regionId', async (req, res) => {
+  const { region_name } = req.body;
+  const regions = await req.context.models.Regions.update(
+    {region_name: region_name},// nama attribute yg akan di update
+    {returning: true,where: { region_id: req.params.regionId } });
 
   return res.send(regions);
 });
