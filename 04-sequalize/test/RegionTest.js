2be29a188c74a8384e16564f7b55a6921acb7a3f
kangdiancp <kangdiancp@gmail.com>
2021-02-11 09:00:35 +0700

    done

--- 04-sequalize/test/RegionTest.js
+++ 04-sequalize/test/RegionTest.js
@@ -2,7 +2,7 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import app from '../index.js';
 
-const should = chai.should();
+//const should = chai.should();
 
 chai.use(chaiHttp);
 
@@ -13,15 +13,20 @@ describe('/GET Regions', () => {
         expect(res.statusCode).toEqual(200);
         expect(res.body.length).toBe(5);
     });
+});
 
+
+describe('/Post Regions', () => {
     // post
     it('should insert a one rows regions, then fetch 6 rows', async () => {
         const res = await chai.request(app).post('/regions/')
             .send({
-                region_name : "Jest Unit"
+                region_name: "Jest Unit"
             });
         expect(res.statusCode).toEqual(200);
         expect(res.body.region_name).toEqual("Jest Unit");
     });
-
 });
+
+
+
---
+++ 99-uml-design/data.js
@@ -0,0 +1,24 @@
+const cartProduct = {
+    "cart_id": 1,
+    "items": [
+        {
+            "prod_id": 1,
+            "qty": 3,
+            "price": 3000
+        },
+        {
+            "prod_id": 2,
+            "qty": 2,
+            "price": 2000
+        },
+        {
+            "prod_id": 3,
+            "qty": 3,
+            "price": 3000
+        },
+        {
+            "prod_id": 4,
+            "qty": 4,
+            "price": 4000
+        }]
+};
